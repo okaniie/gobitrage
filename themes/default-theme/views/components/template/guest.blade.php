21958b6cb4b6a64deb165e2167be081212870f7c
Hero Okani <74093909+okaniie@users.noreply.github.com>
2025-04-23 13:58:39 +0100

    Chapoet reverted

--- themes/default-theme/views/components/template/guest.blade.php
+++ themes/default-theme/views/components/template/guest.blade.php
@@ -16,19 +16,27 @@
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     
     <!-- Begin of Chaport Live Chat code -->
-    <!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '5c2bd584885e000d502870c0a8880cc5e7663e79';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
-        <!-- End of Chaport Live Chat code -->
+        <!-- Begin of Chaport Live Chat code -->
+     <script type="text/javascript">
+         (function(w,d,v3){
+         w.chaportConfig = {
+           appId : '67eb37fb4492a3ec53055f19'
+         };
+     
+         if(w.chaport)return;v3=w.chaport={};v3._q=[];v3._l={};v3.q=function(){v3._q.push(arguments)};v3.on=function(e,fn){if(!v3._l[e])v3._l[e]=[];v3._l[e].push(fn)};var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://app.chaport.com/javascripts/insert.js';
+         
+         // Add error handling
+         s.onerror = function() {
+             console.error('Failed to load Chaport chat script');
+         };
+         
+         s.onload = function() {
+             console.log('Chaport chat script loaded successfully');
+         };
+         
+         var ss=d.getElementsByTagName('script')[0];ss.parentNode.insertBefore(s,ss)})(window, document);
+         </script>
+         <!-- End of Chaport Live Chat code -->
     
     <!-- google translate -->
     <style type="text/css">
